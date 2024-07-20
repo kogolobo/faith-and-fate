1b09d77d602c5af2c82b70513916b189ce63627e
Konstantin Golobokov <kogolobo@uw.edu>
2024-07-19 22:10:04 -0700

    Train with qlora

--- multiplication/train_model.py
+++ multiplication/train_model.py
@@ -1,10 +1,35 @@
 from argparse import ArgumentParser
-from transformers import AutoModelForCausalLM, AutoTokenizer, DataCollatorForLanguageModeling
+import torch
+if torch.cuda.is_available():   
+    torch.backends.cuda.matmul.allow_tf32 = True
+from transformers import AutoModelForCausalLM, AutoTokenizer, DataCollatorForLanguageModeling, BitsAndBytesConfig
 
 from transformers import Trainer, TrainingArguments
 from transformers.trainer_utils import set_seed
 from data_utils import load_data
 from datasets import load_from_disk
+import bitsandbytes as bnb
+
+from peft import (
+    prepare_model_for_kbit_training,
+    LoraConfig,
+    get_peft_model,
+    PeftModel
+)
+from peft.tuners.lora import LoraLayer
+
+def find_all_linear_names(args, model):
+    cls = bnb.nn.Linear4bit
+    lora_module_names = set()
+    for name, module in model.named_modules():
+        if isinstance(module, cls):
+            names = name.split('.')
+            lora_module_names.add(names[0] if len(names) == 1 else names[-1])
+
+
+    if 'lm_head' in lora_module_names: # needed for 16-bit
+        lora_module_names.remove('lm_head')
+    return list(lora_module_names)
 
 def main():
     parser = ArgumentParser()
@@ -23,7 +48,43 @@ def main():
 
     set_seed(args.seed)
 
-    model = AutoModelForCausalLM.from_pretrained(args.model_name)
+    compute_dtype = torch.bfloat16
+    quantization_config = BitsAndBytesConfig(
+            load_in_4bit=True,
+            llm_int8_threshold=6.0,
+            llm_int8_has_fp16_weight=False,
+            bnb_4bit_compute_dtype=compute_dtype,
+            bnb_4bit_use_double_quant=True,
+            bnb_4bit_quant_type="nf4"
+    )
+    torch_dtype = torch.bfloat16
+
+    model = AutoModelForCausalLM.from_pretrained(args.model_name, torch_dtype=compute_dtype, quantization_config=quantization_config, attn_implementation="flash_attention_2")
+    model.config.torch_dtype = compute_dtype
+    model.config.use_cache = False
+    model = prepare_model_for_kbit_training(model, use_gradient_checkpointing=True)
+
+    modules = find_all_linear_names(args, model)
+    lora_config = LoraConfig(
+        r=4,
+        lora_alpha=1,
+        target_modules=modules,
+        lora_dropout=0.1,
+        bias="none",
+        task_type="CAUSAL_LM",
+    )
+    model = get_peft_model(model, lora_config)
+    for name, module in model.named_modules():
+        if isinstance(module, LoraLayer):
+            module = module.to(torch.bfloat16)
+        if 'norm' in name:
+            module = module.to(torch.float32)
+        if 'lm_head' in name or 'embed_tokens' in name:
+            if hasattr(module, 'weight'):
+                if module.weight.dtype == torch.float32:
+                    module = module.to(torch.bfloat16)
+    
+
     tokenizer = AutoTokenizer.from_pretrained(args.model_name)
     collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
     if tokenizer.pad_token is None:
@@ -51,7 +112,8 @@ def main():
         seed=args.seed,
         do_train=True,
         learning_rate=args.learning_rate,
-        load_best_model_at_end=True
+        load_best_model_at_end=True,
+        optim='paged_adamw_32bit'
     )
 
     trainer = Trainer(
